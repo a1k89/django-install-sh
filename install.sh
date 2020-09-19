5ea568c1f1d02add42f6cd0b85d902f7bc44d296
docent-koptev <akoptev1989@yandex.ru>
2020-09-19 18:42:11 +0300

    test

--- install.sh
+++ install.sh
@@ -7,12 +7,14 @@ project_dir=''
 domain_or_ip=''
 user=''
 
+
 read -p "Enter python interpretator: "  base_python_interpretator
 read -p "Enter domain or ip: " domain_or_ip
 read -p "Enter main directory (root directory): " main_dir
 read -p "Enter project directory (where project located): " project_dir
 read -p "User: " user
 
+path_to_logs=$main_dir
 
 cd $main_dir
 `$base_python_interpretator -m venv ienv`
@@ -26,14 +28,16 @@ pip install -r requirements.txt
 cd $main_dir
 mkdir -p "logs"
 
-sed -i "s~{/project_dir/}~$project_dir~g" templates/supervisor/core.conf
-sed -i "s~path_to_logs~$main_dir~g" templates/supervisor/core.conf
+cd $install_dir
 
-sed -i "s~{domain_or_ip}~$domain_or_ip~g" templates/nginx/main.conf
-sed -i "s~{user}~$user~g" templates/nginx/main.conf
+sed -i "s~{project_dir}~$project_dir~g" /templates/supervisor/core.conf
+sed -i "s~{path_to_logs}~$path_to_logs~g" /templates/supervisor/core.conf
 
-sed -i "s~{main_dir}~$main_dir~g" templates/supervisor/celery.conf
-sed -i "s~{project_dir}~$project_dir~g" templates/supervisor/celery.conf
+sed -i "s~{domain_or_ip}~$domain_or_ip~g" /templates/nginx/main.conf
+sed -i "s~{user}~$user~g" /templates/nginx/main.conf
+
+sed -i "s~{main_dir}~$main_dir~g" /templates/supervisor/celery.conf
+sed -i "s~{project_dir}~$project_dir~g" /templates/supervisor/celery.conf
  
 sudo ln -s $install_dir/templates/nginx/main.conf /etc/nginx/sites-enabled/
 sudo ln -s $install_dir/templates/supervisor/core.conf /etc/supervisor/conf.d/
